ab2c1cf7bb61ab5ddb1ed28a3ed1ba43ac400935
Arnold <amolloa@unsa.edu.pe>
2022-07-08 00:00:42 -0500

    Finalizado

--- app/src/main/java/com/danp/danp_push_notification_lab_08/MainActivity.kt
+++ app/src/main/java/com/danp/danp_push_notification_lab_08/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.danp.danp_push_notification_lab_08
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.danp.danp_push_notification_lab_08.ui.theme.DANP_Push_Notification_Lab_08Theme
+import com.danp.danp_push_notification_lab_08.ui.theme.Purple500
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +44,129 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    FirebaseMessaging.getInstance().token
+                        .addOnCompleteListener(OnCompleteListener { task ->
+                            if (!task.isSuccessful) {
+                                Log.d("Notificación FCM", "Error al obtener el token de registro de FCM", task.exception)
+                                return@OnCompleteListener
+                            }
+
+                            val token: String? = task.result
+                            Log.d("Token FCM", token, task.exception)
+                            Toast.makeText(this, token, Toast.LENGTH_SHORT).show()
+                        })
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                text = "Notificaciones FCM",
+                                color = Color.White,
+                                textAlign = TextAlign.Center,
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(Purple500)
+                                    .padding(15.dp)
+                            )
+                        }
+
+                        val extraStadistica: String? = intent.getStringExtra("accion")
+                        var estad: Boolean = false
+                        extraStadistica?.let {
+                            Log.d("etiqueta", extraStadistica.toString())
+                            Estadistica()
+                            estad = true
+                        }
+                        val extraBotones: Boolean = intent.getBooleanExtra("botones", false)
+
+                        if(extraBotones) {
+                            Log.d("etiqueta", extraBotones.toString())
+                            Opciones()
+                            estad = true
+                        }
+
+                        if (!estad)
+                            FirebaseMessaging.getInstance().token
+                                .addOnCompleteListener(OnCompleteListener { task ->
+                                    if (!task.isSuccessful) {
+                                        Log.d("Notificación FCM", "Error al obtener el token de registro de FCM", task.exception)
+                                        return@OnCompleteListener
+                                    }
+
+                                    val token: String? = task.result
+                                })
+                            Principal()
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @Composable
+    fun Principal() {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DANP_Push_Notification_Lab_08Theme {
-        Greeting("Android")
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_lecturas),
+                contentDescription = "Lectura",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(5.dp)
+                    .clip(RoundedCornerShape(20.dp))
+            )
+        }
+    }
+
+    @Composable
+    fun Opciones(){
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Button(onClick = {
+
+            }) {
+                Text(text = "Nueva Lectura")
+            }
+            Button(onClick = {
+
+            }) {
+                Text(text = "Ver registros")
+            }
+        }
+    }
+
+    @Composable
+    fun Estadistica() {
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_estadistica),
+                contentDescription = "Estadistica",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(5.dp)
+                    .clip(RoundedCornerShape(20.dp))
+            )
+        }
     }
 }
+
---
+++ app/src/main/java/com/danp/danp_push_notification_lab_08/service/MyFirebaseMessagingService.kt
@@ -0,0 +1,130 @@
+package com.danp.danp_push_notification_lab_08.service
+
+import android.app.*
+import android.app.Notification.EXTRA_NOTIFICATION_ID
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.danp.danp_push_notification_lab_08.MainActivity
+import com.danp.danp_push_notification_lab_08.R
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.moengage.pushbase.ACTION_SNOOZE
+
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+
+    private val KEY_TEXT_REPLY = "key_text_reply"
+
+    companion object {
+        private const val TAG = "Notificación FCM"
+        const val DEFAULT_NOTIFICATION_ID = 0
+        const val INTENT_REQUEST = 0
+        const val BUTTON_INTENT_REQUEST = 1
+    }
+
+
+    override fun onNewToken(token: String) {
+        Log.i(TAG, "Nuevo token FCM creado: $token")
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        initNotificationChannel(notificationManager)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        val title = message.notification?.title
+        val body = message.notification?.body
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        var notificationBuilder = if(Build.VERSION_CODES.O <= Build.VERSION.SDK_INT) {
+            NotificationCompat.Builder(applicationContext, "1")
+        } else {
+            NotificationCompat.Builder(applicationContext)
+        }
+
+        //Definir intent
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("botones", true)
+        val pendingIntent: PendingIntent? = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(INTENT_REQUEST,  PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        //Botones de acción
+        val buttonIntent1 = Intent (this, MainActivity::class.java)
+        buttonIntent1.putExtra("accion", "presionado")
+        val buttonPendingIntent1 = PendingIntent.getActivity(
+            this,
+            BUTTON_INTENT_REQUEST,
+            buttonIntent1,
+            PendingIntent.FLAG_ONE_SHOT
+        )
+
+        val action1 = NotificationCompat.Action.Builder(
+            R.drawable.ic_lecturas,
+            "Estadísticas",
+            buttonPendingIntent1
+        ).build()
+
+        //Botones de acción
+        val buttonIntent2 = Intent()
+        buttonIntent2.setAction(Intent.ACTION_VIEW)
+        buttonIntent2.setData(Uri.parse("https://revistaganamas.com.pe/tendencias-y-perspectivas-para-el-sector-educacion-en-el-2022/"))
+        val buttonPendingIntent2 = PendingIntent.getActivity(
+            this,
+            BUTTON_INTENT_REQUEST,
+            buttonIntent2,
+            PendingIntent.FLAG_ONE_SHOT
+        )
+
+        val action2 = NotificationCompat.Action.Builder(
+            R.drawable.ic_lecturas,
+            "Referencia",
+            buttonPendingIntent2
+        ).build()
+
+
+        //Construcción de la notificación
+        notificationBuilder = notificationBuilder
+            .setSmallIcon(R.drawable.ic_books)
+            .setContentTitle(title)
+            .setContentText(body)
+            //.setStyle(NotificationCompat.BigTextStyle()
+            //    .bigText(body))
+            .setContentIntent(pendingIntent)
+            .addAction(action1)
+            .addAction(action2)
+            .setAutoCancel(true)
+        initNotificationChannel(notificationManager)
+        notificationManager.notify(DEFAULT_NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+    private fun initNotificationChannel(notificationManager: NotificationManager) {
+        if(Build.VERSION_CODES.O <= Build.VERSION.SDK_INT) {
+            notificationManager.createNotificationChannelIfNotExists(
+                channelId = "1",
+                channelName = "Default"
+            )
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun NotificationManager.createNotificationChannelIfNotExists(
+    channelId: String,
+    channelName: String,
+    importance: Int = NotificationManager.IMPORTANCE_DEFAULT
+) {
+    var channel = this.getNotificationChannel(channelId)
+    if(channel == null){
+        channel = NotificationChannel(
+            channelId,
+            channelName,
+            importance
+        )
+        this.createNotificationChannel(channel)
+    }
+}
